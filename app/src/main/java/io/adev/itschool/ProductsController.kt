77536d5d2457faba6c83ca201ae0bcab0285dea5
Suvorov <als_2991@mail.ru>
2020-05-13 13:30:39 +0500

    create

--- app/src/main/java/io/adev/itschool/ProductsController.kt
+++ app/src/main/java/io/adev/itschool/ProductsController.kt
@@ -14,15 +14,17 @@ data class Product(
     val attributes: List<Attribute>
 ) {
     data class Attribute(
-        val name: String,
-        val value: String
+            val name: String,
+            val value: String
     )
 }
+
 
 data class Category(
         val id: String,
         val tag: String,
         val name: String,
+        val description: String,
         val imageUrl: String
 )
 
@@ -34,67 +36,29 @@ private val categotyListByAutor = mapOf(
                         id = "1",
                         tag = "engin",
                         name = "Машиностроение",
-                        imageUrl ="https://share.ascon.ru/s/kZzYFW4o7yynkMN"
+                        description = "Машиностроение",
+                        imageUrl ="http://chelzakazy.myjino.ru/android/mash.jpg"
                 ),
                 Category(
                         id = "2",
                         tag = "build",
                         name = "Строительство",
-                        imageUrl = "https://share.ascon.ru/s/M9PbpHqncTJHSGq"
+                        description = "Строительство",
+                        imageUrl = "http://chelzakazy.myjino.ru/android/stroit.jpg"
                 )
         )
 )
 
 private val productListsByAuthor = mapOf(
-    "default" to listOf(
-        Product(
-            id = "1",
-            tag = "",
-            name = "Mrkvkv",
-            price = 123.5,
-            discountPercent = 15,
-            description = "Морковка немытая",
-            imageUrl = "https://i.pinimg.com/originals/3e/50/d3/3e50d3c1231de7f7105e017a2ee85874.jpg",
-            attributes = listOf(
-                Product.Attribute(
-                    name = "Качество",
-                    value = "Наивысшее"
-                ),
-                Product.Attribute(
-                    name = "Производитель",
-                    value = "The Grandma Inc."
-                )
-            )
-        ),
-        Product(
-            id = "1",
-            tag = "",
-            name = "Kotoshkv",
-            price = 312.7,
-            discountPercent = 25,
-            description = "Картошка белая",
-            imageUrl = "https://memepedia.ru/wp-content/uploads/2019/07/chilipizdrik-14-360x270.jpg",
-            attributes = listOf(
-                Product.Attribute(
-                    name = "Качество",
-                    value = "Наивысшайшее"
-                ),
-                Product.Attribute(
-                    name = "Производитель",
-                    value = "The Grandma Inc."
-                )
-            )
-        )
-    ),
         "suvorov" to listOf(
                 Product(
                         id = "1",
-                        tag = "ingin",
+                        tag = "engin",
                         name = "КОМПАС 3D",
                         price = 60000.0,
-                        imageUrl = "https://3dtoday.ru/upload/main/095/095bcb543fafbe53813bb2b200370810.png",
+                        imageUrl = "http://3dtoday.ru/upload/main/095/095bcb543fafbe53813bb2b200370810.png",
                         description = "КОМПАС 3D",
-                        discountPercent = 0,
+                        discountPercent = 10,
                         attributes = listOf(
                                 Product.Attribute(
                                         name = "Описание",
@@ -107,26 +71,26 @@ private val productListsByAuthor = mapOf(
                                         name = "Возможности",
                                         value = "* инструменты для коллективной работы, в том числе над проектами, " +
                                                 "содержащими несколько десятков тысяч уникальных компонентов и " +
-                                                "стандартных изделий;\n" +
+                                                "стандартных изделий;\n\n" +
                                                 "* развитый функционал трехмерного твердотельного, поверхностного и " +
-                                                "прямого моделирования;\n" +
+                                                "прямого моделирования;\n\n" +
                                                 "инструменты для работы с исполнениями и конфигурациями " +
-                                                "(в том числе зеркальными) деталей и сборочных единиц;\n" +
+                                                "(в том числе зеркальными) деталей и сборочных единиц;\n\n" +
                                                 "* инструменты моделирования деталей из листового материала с " +
-                                                "последующим автоматическим получением чертежа развертки;\n" +
+                                                "последующим автоматическим получением чертежа развертки;\n\n" +
                                                 "* специальные возможности, облегчающие построение литейных форм: " +
                                                 "литейные уклоны, линии разъема, полости по форме детали " +
-                                                "(в том числе с заданием усадки);\n" +
-                                                "инструменты создания пользовательских библиотек типовых элементов;\n" +
+                                                "(в том числе с заданием усадки);\n\n" +
+                                                "*инструменты создания пользовательских библиотек типовых элементов;\n\n" +
                                                 "* возможность получения технической документации в соответствии " +
                                                 "с ГОСТ, ISO, DIN или стандартами предприятия: чертежи, простые и " +
-                                                "групповые спецификации, отчеты, схемы, таблицы, текстовые документы;\n" +
-                                                "* средства для передачи данных в различные CAD/CAM/CAE-системы;\n" +
+                                                "групповые спецификации, отчеты, схемы, таблицы, текстовые документы;\n\n" +
+                                                "* средства для передачи данных в различные CAD/CAM/CAE-системы;\n\n" +
                                                 "* возможность быстрого перехода от проектирования к изготовлению " +
-                                                "деталей с использованием CAM-систем и станков с ЧПУ;\n" +
+                                                "деталей с использованием CAM-систем и станков с ЧПУ;\n\n" +
                                                 "* возможность простановки размеров, обозначений и технических " +
                                                 "требований в трехмерных моделях (поддержка стандарта ГОСТ " +
-                                                "2.052-2006 «ЕСКД. Электронная модель изделия»).System.lineSeparator()"
+                                                "2.052-2006 «ЕСКД. Электронная модель изделия»)."
                                 )
 
                         )
@@ -134,10 +98,10 @@ private val productListsByAuthor = mapOf(
                 ),
                 Product(
                         id = "2",
-                        tag = "ingin",
+                        tag = "engin",
                         name = "САПР ТП ВЕРТИКАЛЬ",
                         price = 82000.0,
-                        imageUrl = "https://3dtoday.ru/upload/main/095/095bcb543fafbe53813bb2b200370810.png",
+                        imageUrl = "http://3dtoday.ru/upload/main/095/095bcb543fafbe53813bb2b200370810.png",
                         description = "САПР ТР ВЕРТИКАЛЬ",
                         discountPercent = 0,
                         attributes = listOf(
